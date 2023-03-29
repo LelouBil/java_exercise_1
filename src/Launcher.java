fe472750a88a5db6c98dcfee40bdf5de84326f89
Bilel MEDIMEGH <bilel.medimegh@epita.fr>
2023-03-29 14:54:34 +0200

    Partie 5 - freq

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,8 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Launcher {
     public static void main(String[] args) {
@@ -7,14 +11,36 @@ public class Launcher {
         boolean running = true;
         do {
             String input = scanner.nextLine();
-            if (input.equals("quit")) {
-                running = false;
-            } else if (input.equals("fibo")) {
-                fibo(scanner);
-            } else {
-                System.out.println("Unknown command");
+            switch (input) {
+                case "quit" -> running = false;
+                case "fibo" -> fibo(scanner);
+                case "freq" -> freq(scanner);
+                default -> System.out.println("Unknown command");
             }
         } while (running);
+    }
+
+    private static void freq(Scanner scanner) {
+        System.out.println("Chemin du fichier a scanner : ");
+        Path path = Path.of(scanner.nextLine());
+        try {
+            String content = Files.readString(path);
+            content = content
+                    .replaceAll("[^a-zA-Z0-9]"," ")
+                    .toLowerCase();
+            List<String> words = Arrays.stream(content.split(" "))
+                    .filter(s -> !s.isBlank())
+                    .collect(Collectors.groupingBy(s -> s))
+                    .entrySet()
+                    .stream()
+                    .sorted(Comparator.comparingInt((Map.Entry<String,List<String>> e) -> e.getValue().size()).reversed())
+                    .limit(3)
+                    .map(Map.Entry::getKey)
+                    .toList();
+            System.out.println(String.join(" ", words));
+        } catch (IOException e) {
+            System.err.printf("Unreadable file : %s : %s%n", e.getClass().getSimpleName(), e.getMessage());
+        }
     }
 
     private static void fibo(Scanner scanner) {
