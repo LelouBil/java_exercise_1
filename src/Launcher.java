0c7a8f9a5e3bd736689979a8a4db490faafa538d
Bilel MEDIMEGH <bilel.medimegh@epita.fr>
2023-03-29 14:34:34 +0200

    Partie 4 - Fibo

--- src/Launcher.java
+++ src/Launcher.java
@@ -9,9 +9,31 @@ public class Launcher {
             String input = scanner.nextLine();
             if (input.equals("quit")) {
                 running = false;
-            }else{
+            } else if (input.equals("fibo")) {
+                fibo(scanner);
+            } else {
                 System.out.println("Unknown command");
             }
         } while (running);
+    }
+
+    private static void fibo(Scanner scanner) {
+        System.out.print("Nombre : ");
+        int num = scanner.nextInt();
+        scanner.nextLine();
+        int a = 0;
+        int b = 1;
+        if (num == 0) {
+            System.out.println(a);
+        } else if (num == 1) {
+            System.out.println(b);
+        } else {
+            for (int i = 2; i <= num; i++) {
+                int n = a + b;
+                a = b;
+                b = n;
+            }
+            System.out.println(b);
+        }
     }
 }
